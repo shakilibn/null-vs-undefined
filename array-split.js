e38463ae97901796f919b41ac078da627fb02d71
shakilibn <mdsh540@gmail.com>
2021-02-02 20:39:53 +0600

    third commit

---
+++ array-split.js
@@ -0,0 +1,11 @@
+const nums = [1,2,3,4,5,6,7,8,9];
+// const part = nums.slice(2,6);
+// console.log(part);
+// console.log(nums);
+
+// const part = nums.splice(2,0,77,88,99);
+// console.log(part);
+// console.log(nums);
+
+const part = nums.join(" ");
+console.log(part);
