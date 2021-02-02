d04211ce7fbe2e059a89589cae64f73bc3dd7d34
shakilibn <mdsh540@gmail.com>
2021-02-02 21:18:49 +0600

    first-commit

---
+++ break-continue.js
@@ -0,0 +1,14 @@
+// const nums = [1,2,3,4,5,6,7,8,9];
+// for (let i = 0; i < nums.length; i++) {
+//     const element = nums[i];
+//     if(element ==4){
+//         continue;
+//     }
+//     console.log(element);
+    
+// }
+
+function doSomething(x, y = 4){
+    console.log(y);    
+    }    
+    doSomething(3,2);
---
+++ if-else.js
@@ -0,0 +1,8 @@
+const age = '0';
+
+if(1){
+    console.log("condition is true");
+}
+else {
+    console.log("condition is false");
+}
